4e88ce0df13819d9bca07daa70050697faba6706
Dominick <cowboysfan0888@gmail.com>
2020-05-22 07:49:47 -0400

    new commit

--- GeneralBook.java
+++ GeneralBook.java
@@ -1,10 +1,9 @@
-package Library;
-
 public class GeneralBook extends Book {
 
     public GeneralBook(int publishYear, String title, String author, String genre, String ageGroup, boolean isAvailable,
-            boolean isFactual) {
-        super(publishYear, title, author, genre, ageGroup, isAvailable, isFactual);
+            boolean isFactual2, BookCondition condition, String authorFirstName, String authorLastName) {
+        super(publishYear, title, author, genre, ageGroup, isAvailable, isFactual2, condition, authorFirstName,
+                authorLastName);
     }
 
 }
--- LibraryItem.java
+++ LibraryItem.java
@@ -1,21 +1,28 @@
-package Library;
-
-public class LibraryItem {
+public class LibraryItem implements Reservable, Loanable {
 
     private int publishYear;
-    private String title, author, genre, ageGroup;
-    private boolean isAvailable, isFactual;
+    private String title, genre, ageGroup;
+    private boolean isAvailable, isFactual2;
+    private BookCondition condition;
+    public Author author = new Author();
+    String firstName = author.getFirstName(), lastName = author.getLastName();
+    public PublishingCompany publishingCompany = new PublishingCompany();
 
     // constructor
-    public LibraryItem(int publishYear, String title, String author, String genre, String ageGroup, boolean isAvailable,
-            boolean isFactual) {
+    public LibraryItem(int publishYear, String title, Author author, String genre, String ageGroup, boolean isAvailable,
+            boolean isFactual2, BookCondition condition, String firstName, String lastName,
+            PublishingCompany publishingCompany) {
         this.publishYear = publishYear;
         this.title = title;
-        this.author = author;
+        // this.author = author;
         this.genre = genre;
         this.ageGroup = ageGroup;
         this.isAvailable = isAvailable;
-        this.isFactual = isFactual;
+        this.isFactual2 = isFactual2;
+        this.condition = condition;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.publishingCompany = publishingCompany;
     }
 
     // getters
@@ -28,9 +35,9 @@ public class LibraryItem {
         return title;
     }
 
-    public String getAuthor() {
-        return author;
-    }
+    // public String getAuthor() {
+    // return author;
+    // }
 
     public String getGenre() {
         return genre;
@@ -45,7 +52,11 @@ public class LibraryItem {
     }
 
     public boolean getFactuality() {
-        return isFactual;
+        return isFactual2;
+    }
+
+    public BookCondition getCondition() {
+        return condition;
     }
 
     // setters
@@ -58,9 +69,9 @@ public class LibraryItem {
         this.title = title;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    // public void setAuthor(String author) {
+    // this.author = Author.getFirstName();
+    // }
 
     public void setGenre(String genre) {
         this.genre = genre;
@@ -74,27 +85,82 @@ public class LibraryItem {
         this.isAvailable = isAvailable;
     }
 
-    public void setFactuality(boolean isFactual) {
-        this.isFactual = isFactual;
+    public void setFactuality(boolean isFactual2) {
+        this.isFactual2 = isFactual2;
     }
 
-    public LibraryItem(boolean isAvailable, boolean isFactual) {
+    public void setCondition(BookCondition condition) {
+        this.condition = condition;
+    }
+
+    public LibraryItem(boolean isAvailable, boolean isFactual2) {
         if (isAvailable) {
             System.out.println("Yes, the book " + getTitle() + " is available");
         } else {
             System.out.println("Sorry, " + getTitle() + " is not available");
         }
-        if (isFactual) {
+        if (isFactual2) {
             System.out.println("Reference book or Newspaper");
         } else {
             System.out.println("General book or Magazine");
         }
     }
 
+    public LibraryItem(int publishYear2, String title2, String author2, String genre2, String ageGroup2,
+            boolean isAvailable2, boolean isFactual22, BookCondition condition2, String authorFirstName,
+            String authorLastName) {
+    }
+
     @Override // Annotation telling us and compiler that we are Overriding previously defined
               // behavior
     public String toString() {
-        return this.getTitle() + " by " + this.getAuthor() + " published " + this.getPublishYear();
+        return this.getTitle() + " by " + author.getFirstName() + author.getLastName() + " published on "
+                + this.getPublishYear() + " by " + publishingCompany.getPublishingCompany();
+    }
+
+    @Override
+    public boolean isLoanable() {
+        return true;
+    }
+
+    public String getLoanStatus() {
+        return null;
+    }
+
+    public int getMaximumLoanDays() {
+        return 10;
+    }
+
+    public boolean isReservable() {
+        return true;
+    }
+
+    public int getReserveTerm() {
+        return 15;
+    }
+
+    public int getMaxReservePerYear() {
+        return 1000;
+    }
+
+    @Override
+    public void lateFee(Charges fee) {
+
+        switch (fee) {
+            case DAILY:
+                System.out.println("Late Fee: $1");
+                break;
+            case WEEKLY:
+                System.out.println("Late Fee: $7");
+                break;
+            case BIWEEKLY:
+                System.out.println("Late Fee: $20");
+                break;
+            case MONTHLY:
+                System.out.println("Late Fee: $50");
+            default:
+                break;
+        }
     }
 
 }
